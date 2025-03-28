a0bb4da087c8463807aa2c42dfedf76f9eefe66f
Lysenti <eren926@gmail.com>
2025-03-28 11:56:08 +0100

    Mise en page de la page press.js et du css

--- src/components/Press.js
+++ src/components/Press.js
@@ -4,9 +4,28 @@ import '../styles/Press.css';
 
 const Press = () => {
   return (
-    <div>
-      <h1>Presse</h1>
-      <p>C'est ici que le contenu de la page Presse sera affiché.</p>
+    <div
+      className="press-container"
+      style={{
+        backgroundImage: "url('/assets/background.png')",
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+        minHeight: '100vh',
+        position: 'relative',
+      }}
+    >
+
+      {/* Images décoratives */}
+      <img className="image2-overlay" src="/assets/image2.png" alt="Décoration 2" />
+      <img className="image3-overlay" src="/assets/image3.jpg" alt="Décoration 3" />
+
+      {/* Images de presse */}
+      <img className="press-image press-1" src="/assets/presse_1.png" alt="Article 1" />
+      <img className="press-image press-2" src="/assets/presse_2.png" alt="Article 2" />
+      <img className="press-image press-3" src="/assets/presse_3.png" alt="Article 3" />
+      <img
+      className="gif-facing" src="/assets/exe-facing-couleurs-SDG.gif" alt="GIF décoratif en bas à droite"/>
     </div>
   );
 };
