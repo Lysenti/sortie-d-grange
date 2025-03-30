d70c2958b24cd0bdb3ea00260386a72702560338
Lysenti <eren926@gmail.com>
2025-03-30 13:38:43 +0200

    Intégration page Shop et mise en forme

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -4,11 +4,61 @@ import '../styles/Shop.css';
 
 const Shop = () => {
   return (
-    <div>
-      <h1>Boutique</h1>
-      <p>C'est ici que le contenu de la boutique sera affiché.</p>
+    <div
+      className="shop-container"
+      style={{
+        backgroundImage: "url('/assets/arriere_sdg_couleur.jpg')",
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        minHeight: '100vh',
+        padding: '40px 20px'
+      }}
+    >
+      {/* Lien SoundCloud */}
+      <div className="shop-soundcloud">
+        <iframe
+          width="100%"
+          height="120"
+          allow="autoplay"
+          src="https://w.soundcloud.com/player/?url=https%3A//soundcloud.com/ogusto-1/sets/corrosion-superficielle&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"
+        ></iframe>
+      </div>
+
+      {/* Articles */}
+      <div className="shop-items">
+        <div className="shop-item">
+          <div className="shop-icon">
+            <img src="/assets/image2.png" alt="Article 1" />
+          </div>
+          <div className="shop-description">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
+          </div>
+          <div className="shop-status available">✔</div>
+        </div>
+
+        <div className="shop-item">
+          <div className="shop-icon">
+            <img src="/assets/image3.jpg" alt="Article 2" />
+          </div>
+          <div className="shop-description">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
+          </div>
+          <div className="shop-status available">✔</div>
+        </div>
+
+        <div className="shop-item">
+          <div className="shop-icon">
+            <img src="/assets/logo.png" alt="Article 3" />
+          </div>
+          <div className="shop-description">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
+          </div>
+          <div className="shop-status unavailable">❌</div>
+        </div>
+      </div>
     </div>
   );
 };
 
 export default Shop;
+
