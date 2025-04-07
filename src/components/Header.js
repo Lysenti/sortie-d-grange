1fd8d6d222b83be000c0f62dd6341da90a8c7e5f
Lysenti <eren926@gmail.com>
2025-04-07 15:05:15 +0200

    Rectifications styles du header et de l'affichage du Grange.css

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,21 +6,23 @@ import '../styles/Header.css';
 const Header = () => {
   return (
     <header className="navigation">
-      <div className="logo">
-        <Link to="/">
-          <img src="/assets/logo.png" alt="Logo" />
-        </Link>
+      <div className="navigation-inner">
+        <div className="logo">
+          <Link to="/">
+            <img src="/assets/logo.png" alt="Logo" />
+          </Link>
+        </div>
+        <nav>
+          <ul>
+            <li><Link to="/">Accueil</Link></li>
+            <li><Link to="/press">Presse</Link></li>
+            <li><Link to="/lyrics">Paroles</Link></li>
+            <li><Link to="/shop">Boutique</Link></li>
+            <li><Link to="/partners">Partenariats</Link></li>
+            <li><Link to="/contact">Contacts</Link></li>
+          </ul>
+        </nav>
       </div>
-      <nav>
-        <ul>
-          <li><Link to="/">Accueil</Link></li>
-          <li><Link to="/press">Presse</Link></li>
-          <li><Link to="/lyrics">Paroles</Link></li>
-          <li><Link to="/shop">Boutique</Link></li>
-          <li><Link to="/partners">Partenariats</Link></li>
-          <li><Link to="/contact">Contacts</Link></li>
-        </ul>
-      </nav>
     </header>
   );
 };
